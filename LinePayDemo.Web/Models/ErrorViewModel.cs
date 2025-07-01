0246dd8635edf69244e3ee751c4b953294eb21c4
zoozxc456 <zoozxc456@gmail.com>
2025-07-01 12:23:00 +0800

    feat: initial project

---
+++ LinePayDemo.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LinePayDemo.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
